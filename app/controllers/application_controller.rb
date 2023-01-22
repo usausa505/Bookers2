d7ef26d35cd3d080caf89d06b91afc7abc30ad61
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-22 05:10:01 +0000

    [Check point] Bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
 
   protected
@@ -15,5 +15,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
+
 end
