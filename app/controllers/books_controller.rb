1ac556dc685a1a80f7be16a9a9231f6316de8939
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-23 13:58:23 +0000

    [Check point] Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,15 +6,10 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @user = User.find(params[:id])
-    if @book.save
-      flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render :index
-    end
+    @book.save
+    redirect_to books_path
   end
+  
   def index
     @books = Book.all
     @book = Book.new
