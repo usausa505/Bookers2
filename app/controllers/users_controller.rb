948177898e2189159ca610785e7d708f283b78cf
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-25 03:02:12 +0000

    [Check point] Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,15 @@ class UsersController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
