acf04e9d0f6b2c8d3a0f7b211b8b553662821667
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-25 08:51:28 +0000

    [Check point] Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-  get '/homes/about', as: 'about'
+  get '/home/about', to: 'homes#about', as: 'about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
